d0b98bf0db12c84a00c8ce6b90ac82580459537f
Spencer Neukam <neukams@oregonstate.edu>
2023-09-14 19:14:19 -0400

    Same update as the last commit (New Functionality); but forgot to add the greetings module.

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -23,6 +23,30 @@ func Hello(name string) (string, error) {
 	return message, nil
 }
 
+// Hellos() reutrns a map that associates each of the named people
+// with a greeting message.
+func Hellos(names []string) (map[string]string, error) {
+
+	// a map to associate names with messages
+	messages := make(map[string]string)
+
+	// loop through received slice of names,
+	// calling the Hello() function to get a message for each name.
+	for _, name := range names {
+		greeting_message, err := Hello(name)
+
+		// return if error (error = empty name)
+		if err != nil {
+			return nil, err
+		}
+
+		// otherwise, capture the greeting for the user's name
+		messages[name] = greeting_message
+	}
+
+	return messages, nil
+}
+
 // return a random greeting message
 func randomFormat() string {
 
